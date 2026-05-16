0beefa6d53a7b2cc699045e2da34ebc328791035
ailpro1 <fudhail@talonget.com>
2026-05-16 10:49:12 +0800

    Update file paths in service worker cache

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 const CACHE = 'af-inspection-v1';
 const FILES = [
-  './af_home_inspection_app.html',
+  '/AFH/af_home_inspection_app.html',
+  '/AFH/manifest.json',
+  '/AFH/icon-180.png',
   'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400;500;600;700&family=DM+Sans:wght@300;400;500;600&family=Playfair+Display:wght@400;700&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
